8c577434b4fd0219a872a9787ac03285a0a247c8
MMeskhi <mishameskhi1@gmail.com>
2022-11-02 16:46:07 +0400

    finished main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,44 @@
-//Navbar dropdown
-const menuAnchor = document.querySelectorAll(".nav-right ul li a");
-
-menuAnchor.forEach((btn) => {
-  btn.addEventListener("mouseover", () => {
-    const parentMenu = btn.closest(".nav-right ul li");
-    const menuOpen = parentMenu.querySelector(".dropdown");
-    const filterToggle = () => {
-      menuOpen.classList.toggle("inactive");
-    };
-    filterToggle(btn);
-  });
-});
-
 //Mobile sidebar
 const mobileHamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector(".mobile-sidebar");
 const noScroll = document.querySelector("body");
 const sidebarMenu = document.querySelector(".mobile-menu");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
 
 function openSidebar() {
   sidebar.classList.toggle("sidebar-active");
   noScroll.classList.toggle("no-scroll");
   sidebarMenu.classList.toggle("active");
+  menuOpen.classList.toggle("switch-btn");
+  menuClose.classList.toggle("switch-btn");
 }
 
 mobileHamburger.addEventListener("click", openSidebar);
+
+const mobileCatTogglers = document.querySelectorAll(".mobile-cat");
+// const mobileDropdown = document.querySelectorAll(".mobile-menu-dropdown");
+
+mobileCatTogglers.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const parentFilter = btn.closest(".mobile-cat");
+    const filterOpen = parentFilter.querySelector(".mobile-menu-dropdown");
+    const filterToggle = () => {
+      filterOpen.classList.toggle("open");
+    };
+    filterToggle(btn);
+  });
+});
+
+//Lang switcher dropdown
+const langSwitch = document.querySelector(".lang-swt");
+const langDropdown = document.querySelector(".lang-swt-drp");
+
+function openLangSwitcher() {
+  langSwitch.classList.toggle("lang-active");
+  langDropdown.classList.toggle("swt-active");
+}
+
+langSwitch.addEventListener("click", openLangSwitcher);
+
+//Click outside of mobile sidebar and lang switcher
