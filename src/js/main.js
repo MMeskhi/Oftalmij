ebb71f6af9b1fd1eb63ebe7c5d8df3a097aa1d76
Mikheil Meskhi <mishameskhi1@gmail.com>
2022-11-09 17:39:19 +0400

    update branches page and lang switcher

--- src/js/main.js
+++ src/js/main.js
@@ -58,73 +58,17 @@ function isLangSwitcherOpen() {
 }
 
 // Map hover
-if (document.getElementById("branches")) {
-  const branchSelectorZugdidi = document.getElementById("zug");
-  const branchHoverZugdidi = document.querySelector(".zugdidi");
-  const branchSelectorKutaisi = document.getElementById("kut");
-  const branchHoverKutaisi = document.querySelector(".kutaisi");
-  const branchSelectorGori = document.getElementById("gori");
-  const branchHoverGori = document.querySelector(".gori");
-  const branchSelectorTelavi = document.getElementById("tela");
-  const branchHoverTelavi = document.querySelector(".telavi");
-  const branchSelectorTbilisi = document.getElementById("tbil");
-  const branchHoverTbilisi = document.querySelector(".tbilisi");
-  const branchSelectorMarneuli = document.getElementById("marn");
-  const branchHoverMarneuli = document.querySelector(".marneuli");
-  const branchSelectorRustavi = document.getElementById("rust");
-  const branchHoverRustavi = document.querySelector(".rustavi");
-  const branchSelectorGurjani = document.getElementById("gurj");
-  const branchHoverGurjani = document.querySelector(".gurjani");
+const branchesBlocks = document.querySelectorAll(".branch-grid");
+const mapBranches = document.querySelectorAll(".map-hover ");
 
-  function selectBranch() {
-    branchSelectorZugdidi.classList.toggle("branch-selected");
-  }
-  function selectBranchB() {
-    branchSelectorKutaisi.classList.toggle("branch-selected");
-  }
-  function selectBranchC() {
-    branchSelectorGori.classList.toggle("branch-selected");
-  }
-  function selectBranchD() {
-    branchSelectorTelavi.classList.toggle("branch-selected");
-  }
-  function selectBranchE() {
-    branchSelectorTbilisi.classList.toggle("branch-selected");
-  }
-  function selectBranchF() {
-    branchSelectorMarneuli.classList.toggle("branch-selected");
-  }
-  function selectBranchG() {
-    branchSelectorRustavi.classList.toggle("branch-selected");
-  }
-  function selectBranchH() {
-    branchSelectorGurjani.classList.toggle("branch-selected");
-  }
-
-  branchHoverZugdidi.addEventListener("mouseover", selectBranch, false);
-  branchHoverZugdidi.addEventListener("mouseout", selectBranch, false);
-
-  branchHoverKutaisi.addEventListener("mouseover", selectBranchB, false);
-  branchHoverKutaisi.addEventListener("mouseout", selectBranchB, false);
-
-  branchHoverGori.addEventListener("mouseover", selectBranchC, false);
-  branchHoverGori.addEventListener("mouseout", selectBranchC, false);
-
-  branchHoverTelavi.addEventListener("mouseover", selectBranchD, false);
-  branchHoverTelavi.addEventListener("mouseout", selectBranchD, false);
-
-  branchHoverTbilisi.addEventListener("mouseover", selectBranchE, false);
-  branchHoverTbilisi.addEventListener("mouseout", selectBranchE, false);
-
-  branchHoverMarneuli.addEventListener("mouseover", selectBranchF, false);
-  branchHoverMarneuli.addEventListener("mouseout", selectBranchF, false);
-
-  branchHoverRustavi.addEventListener("mouseover", selectBranchG, false);
-  branchHoverRustavi.addEventListener("mouseout", selectBranchG, false);
-
-  branchHoverGurjani.addEventListener("mouseover", selectBranchH, false);
-  branchHoverGurjani.addEventListener("mouseout", selectBranchH, false);
-}
+mapBranches.forEach((pin, index) => {
+  pin.addEventListener("mouseover", () => {
+    branchesBlocks[index].classList.add("branch-selected");
+  });
+  pin.addEventListener("mouseout", () => {
+    branchesBlocks[index].classList.remove("branch-selected");
+  });
+});
 
 //Contact from
 const contactWarning = document.querySelector(".contact-warning");
